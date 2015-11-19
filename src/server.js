786519b8a63e9d199a69daf67d74783269575acd
Kevin Pruett <pruett.kevin@gmail.com>
2015-11-19 12:06:21 -0500

    start to integrate hapi

--- src/server.js
+++ src/server.js
@@ -1,15 +1,33 @@
-import Server from 'socket.io'
-const port = process.env.PORT || 8090
+// import Server from 'socket.io'
+// const port = process.env.PORT || 8090
 
-export function startServer (store) {
-  const io = new Server().attach(port)
+// export function startServer (store) {
+//   const io = new Server().attach(port)
 
-  store.subscribe(
-    () => io.emit('state', store.getState().toJS())
-  )
+//   store.subscribe(
+//     () => io.emit('state', store.getState().toJS())
+//   )
 
-  io.on('connection', (socket) => {
-    socket.emit('state', store.getState().toJS())
-    socket.on('action', store.dispatch.bind(store))
-  })
-}
+//   io.on('connection', (socket) => {
+//     socket.emit('state', store.getState().toJS())
+//     socket.on('action', store.dispatch.bind(store))
+//   })
+// }
+
+import Hapi from 'hapi'
+import r from 'rethinkdb'
+
+const server = new Hapi.Server()
+
+server.connection({ port: 1234 })
+
+server.route({
+  method: 'GET',
+  path: '/echo',
+  handler (request, reply) {
+    console.log(request.headers.host)
+    reply()
+  }
+})
+
+server.start(() => {})
